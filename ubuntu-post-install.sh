213447e5d32672df864b12f49b6d6376f1428f75
jamiemcg <jamiemcgowan.dev@gmail.com>
2015-03-17 23:29:34 +0000

    Initial commit

---
+++ ubuntu-post-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Author: Jamie McGowan
+# Description: Post installation script for Ubuntu
+#
+# DELETE ANY NOTES WHEN FINISHED
+# Notes: 
+# Universe repository:		sudo add-apt-repository universe
